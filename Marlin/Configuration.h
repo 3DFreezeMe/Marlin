5b140d14c60ae9d9f8547050de451f2377724cc7
3DFreezeMe <30751546+3DFreezeMe@users.noreply.github.com>
2017-08-30 17:41:36 +0200

    removed custom bootscreen (SD card init issue)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -90,7 +90,7 @@
 // boot image unmodified. For an example have a look at the bq Hephestos 2
 // example configuration folder.
 //
-#define SHOW_CUSTOM_BOOTSCREEN // 3DFreezeMe
+//#define SHOW_CUSTOM_BOOTSCREEN
 // @section machine
 
 /**
